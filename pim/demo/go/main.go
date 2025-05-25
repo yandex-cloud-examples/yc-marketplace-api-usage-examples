720bcfdb2d1641f808aaf54a8cc82289148495d0
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2025-05-25 21:21:56 +0200

    feat(pim): add handlers for product instance binding and usage reporting

--- pim/demo/go/main.go
+++ pim/demo/go/main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	ycsdk "github.com/yandex-cloud/go-sdk"
+	"github.com/yandex-cloud/go-sdk/iamkey"
+	"github.com/yandex-cloud/go-sdk/pkg/requestid"
+	"google.golang.org/grpc"
 
 	"demo/pkg/db"
 )
@@ -24,11 +27,38 @@ func main() {
 		log.Fatalf("Could not connect to database: %s\n", err)
 	}
 
+	var credentials ycsdk.Credentials
+	saKeyFile := os.Getenv("YC_SA_KEY_FILE")
+	if saKeyFile != "" {
+		fileData, err := os.ReadFile(saKeyFile)
+		if err != nil {
+			log.Fatalf("Could not read service account key file: %s\n", err)
+			return
+		}
+		var saKey iamkey.Key
+		if err := saKey.UnmarshalJSON(fileData); err != nil {
+			log.Fatalf("Could not unmarshal service account key: %s\n", err)
+			return
+		}
+		credentials, err = ycsdk.ServiceAccountKey(&saKey)
+		if err != nil {
+			log.Fatalf("Could not create service account credentials: %s\n", err)
+			return
+		}
+	} else {
+		credentials = ycsdk.InstanceServiceAccount()
+		log.Println("Using instance service account credentials")
+	}
+
 	cfg := ycsdk.Config{
-		Credentials: ycsdk.InstanceServiceAccount(),
+		Credentials: credentials,
 	}
 	ctx := context.Background()
-	sdk, err := ycsdk.Build(ctx, cfg)
+	sdk, err := ycsdk.Build(
+		ctx,
+		cfg,
+		grpc.WithUnaryInterceptor(requestid.Interceptor()),
+	)
 	if err != nil {
 		log.Fatal("SDK init error: " + err.Error())
 	}
@@ -43,6 +73,10 @@ func main() {
 	r.Post("/login", s.loginPostHandler)
 
 	r.Get("/logout", s.logoutHandler)
+
+	r.Post("/bind", s.bindPostHandler)
+
+	r.Post("/report", s.reportPostHandler)
 
 	r.Get("/", s.indexHandler)
 
--- pim/demo/go/user.go
+++ pim/demo/go/user.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"demo/pkg/db"
 
+	"github.com/google/uuid"
+	"github.com/yandex-cloud/go-genproto/yandex/cloud/marketplace/metering/v1"
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/marketplace/pim/v1/saas"
 	ycsdk "github.com/yandex-cloud/go-sdk"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // Embed the entire directory.
@@ -199,9 +204,63 @@ func (s Server) indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s Server) handleReport(w http.ResponseWriter, r *http.Request) {
+func (s Server) bindPostHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), 20*time.Second)
+	defer cancel()
+	user, token := s.reqVariables(r)
+	if user == nil || token == "" {
+		http.Error(w, "User not logged in or token missing", http.StatusBadRequest)
+		return
+	}
+	op, err := s.sdk.WrapOperation(s.sdk.Marketplace().PIM().ProductInstance().Claim(r.Context(), &saas.ClaimProductInstanceRequest{
+		Token: token,
+	}))
+	// Wait for the operation to complete
+	err = op.Wait(ctx)
+	if err != nil {
+		return
+	}
+	resp, err := op.Response()
+	if err != nil {
+		log.Printf("Could not claim product instance: %s\n", err)
+		http.Error(w, "Invalid token", http.StatusBadRequest)
+		return
+	}
+	pim, ok := resp.(*saas.ProductInstance)
+	if !ok {
+		log.Printf("Invalid response type: %T\n", resp)
+		http.Error(w, "Invalid token", http.StatusBadRequest)
+		return
+	}
+	err = s.repo.UpdateUser(r.Context(), user.Login, pim.Id)
+	w.Header().Set("Location", "/")
+	w.WriteHeader(http.StatusSeeOther)
+}
+
+func (s Server) reportPostHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the user and token from the request
 	user, _ := s.reqVariables(r)
+
+	// Read body and parse it as form
+	if user == nil || user.ProductInstanceId == "" {
+		http.Error(w, "User not logged in or product instance not bound", http.StatusBadRequest)
+		return
+	}
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	// Get amount from body
+	amountValue := r.FormValue("amount")
+	if amountValue == "" {
+		http.Error(w, "Amount is required", http.StatusBadRequest)
+		return
+	}
+	amount, err := strconv.Atoi(amountValue)
+	if err != nil || amount <= 0 {
+		http.Error(w, "Invalid amount", http.StatusBadRequest)
+		return
+	}
 
 	inst, err := s.sdk.Marketplace().PIM().ProductInstance().Get(r.Context(), &saas.GetProductInstanceRequest{
 		ProductInstanceId: user.ProductInstanceId,
@@ -211,6 +270,38 @@ func (s Server) handleReport(w http.ResponseWriter, r *http.Request) {
 	}
 	if inst == nil {
 		http.Error(w, "Product instance not found", http.StatusNotFound)
+		return
+	}
+
+	recordId, err := uuid.NewUUID()
+	if err != nil {
+		log.Printf("Could not generate UUID for usage record: %s\n", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	// This is a hardcoded SKU ID for the demo. In a real application, you would retrieve this from the product instance or configuration.
+	const skuId = "dn2e395635hoblkrj1at"
+	resp, err := s.sdk.Marketplace().Metering().ProductUsage().Write(r.Context(), &metering.WriteUsageRequest{
+		DryRun:            false,
+		ProductInstanceId: user.ProductInstanceId,
+		UsageRecords: []*metering.UsageRecord{
+			{
+				Uuid:     recordId.String(),
+				SkuId:    skuId,
+				Quantity: int64(amount),
+				Timestamp: &timestamppb.Timestamp{
+					Seconds: time.Now().Unix(),
+					Nanos:   0,
+				},
+			},
+		},
+	})
+	if err != nil {
+		return
+	}
+	if len(resp.Rejected) > 0 {
+		log.Printf("Usage record rejected: %v\n", resp.Rejected)
+		http.Error(w, "Usage record rejected", http.StatusBadRequest)
 		return
 	}
 
